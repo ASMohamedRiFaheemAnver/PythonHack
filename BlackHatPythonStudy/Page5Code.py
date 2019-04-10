9da34b789907e565c74afb77eed2f6380f5ec954
ubunturifa <jstrfaheem065@gmail.com>
2019-04-10 19:41:11 +0530

    Added HackerRank Python Studies <3,

---
+++ BlackHatPythonStudy/Page5Code.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+def sum(numberone, numbertwo):
+    numberoneint = convertointeger(numberone)
+    numbertwoint = convertointeger(numbertwo)
+
+    return numberoneint + numbertwoint
+
+def convertointeger(numberstring):
+    return (int)(numberstring)
+
+print(sum("1", "4"))
