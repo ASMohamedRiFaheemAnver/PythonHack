76d5b3da2225467d493668fe5e7da47a358e6bab
ubunturifa <jstrfaheem065@gmail.com>
2019-04-11 21:46:11 +0530

    TCPClient and Server <3,

--- BlackHatPythonStudy/TCPClient/SoketProgrammingGeeks.py
+++ BlackHatPythonStudy/TCPClient/SoketProgrammingGeeks.py
@@ -24,7 +24,7 @@ print(ip)
 """
 
 #Handling exception
-
+"""
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print("Socket successfully created")
@@ -45,3 +45,51 @@ except socket.gaierror:
 #https://stackoverflow.com/questions/19143091/typeerror-connect-takes-exactly-one-argument
 s.connect((hostIp, port))
 print("The socket has successfully connected to google on port : %s"% hostIp)
+"""
+
+#Server Program
+"""
+#Creating socket object
+s = socket.socket()
+print("Socket successfully created")
+
+port = 12345
+
+#Make sever to listen to requests
+s.bind(('', port))
+print("Socket binded to %s"% (port))
+
+#Put the socket into listening mode
+s.listen(5)
+print("Socket is listening")
+
+#A forever loop
+while True:
+    #Establish connection with client
+    c, addr = s.accept()
+    print("Got connction from", (addr))
+
+    #Sending a thank you message to the client
+    #Got error with python 3
+    #https://stackoverflow.com/questions/33003498/typeerror-a-bytes-like-object-is-required-not-str
+    c.send("Thank you for connecting".encode())
+    #Close the connection with the client
+    c.close() 
+"""
+
+#Client Program
+
+# Create a socket object 
+s = socket.socket()          
+  
+# Define the port on which you want to connect 
+port = 12345                
+  
+# connect to the server on local computer 
+s.connect(('127.0.0.1', port)) 
+  
+# receive data from the server 
+print(s.recv(1024))
+
+# close the connection 
+s.close()    
--- BlackHatPythonStudy/TCPClient/TCPClient.py
+++ BlackHatPythonStudy/TCPClient/TCPClient.py
@@ -3,11 +3,18 @@ import socket
 targetHost = "www.google.com"
 targerPort = 80
 
-#create a socket object
+#create socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connect the client
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.connect((targetHost, targerPort))
+
+#send some data
+data = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+client.send(data.encode("utf-8"))
 
 #receive some data
 response = client.recv(4096)
+
+#printing the response
+print(response)
