29698ee0086696b3099c86cb599bc038b611010f
ubunturifa <jstrfaheem065@gmail.com>
2019-04-12 15:50:09 +0530

    PythonicUDP server client code and png <3,

---
+++ BlackHatPythonStudy/TCPandUDP/PythonicUDPClient.py
@@ -0,0 +1,18 @@
+#From https://pythontic.com/modules/socket/udp-client-server-example
+
+import socket
+
+msgFromClent = "Hello UDP Server"
+byteToSend = str.encode(msgFromClent)
+serverAddressPort = ("127.0.0.1", 20001)
+bufferSize = 1024
+
+#Create a UDP socket at client side
+UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Send msg to server
+UDPClientSocket.sendto(byteToSend, serverAddressPort)
+
+msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+
+print(msgFromClent)
+UDPClientSocket.close()
---
+++ BlackHatPythonStudy/TCPandUDP/PythonticUDPserver.py
@@ -0,0 +1,26 @@
+#From https://pythontic.com/modules/socket/udp-client-server-example
+
+import socket
+
+localIP = "127.0.0.1"
+localPort = 20001
+bufferSize = 1024
+
+#Initializing encoding to byte
+msgFromServer = "Hello UDP Client"
+byteToSend = str.encode(msgFromServer)
+
+#Create a datagram socket
+UDPServerSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Bind to Address and IP
+UDPServerSocket.bind((localIP, localPort))
+print("UDP Server up and listening")
+
+#Listen for incoming datagrams
+
+while(True):
+    byteAddressPair = UDPServerSocket.recvfrom(bufferSize)
+    print(byteAddressPair)
+    #Sending a replay to client
+    UDPServerSocket.sendto(byteToSend, byteAddressPair[1])
