bf4582ccb27b142fadb65384f9f22112d720af67
ubunturifa <jstrfaheem065@gmail.com>
2019-04-13 00:17:58 +0530

    For loop <3,

---
+++ HackerRankStudy/ForLoop.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+
+counter = 0
+while(counter < n):
+    print(counter*counter)
+    counter = counter + 1
+# https://www.w3schools.com/python/python_for_loops.asp
+for x in range(n):
+    print(x*x)
