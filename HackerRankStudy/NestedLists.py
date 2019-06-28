bef69e68f392e7d036badb62b35cfdc36db57e73
ubunturifa <jstrfaheem065@gmail.com>
2019-06-28 14:47:21 +0530

    Added List code,

---
+++ HackerRankStudy/NestedLists.py
@@ -0,0 +1,12 @@
+# https://www.hackerrank.com/challenges/nested-list/tutorial
+
+nested_list = [['blue', 'green'], ['red', 'black'], ['blue', 'white']]
+
+# getting the length
+print(len(nested_list))
+
+# printing first element set
+print(nested_list[0])
+
+# printing first element
+print(nested_list[0][0])
