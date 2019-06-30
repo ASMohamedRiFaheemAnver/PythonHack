4682129f34292ced797b9144a694715b805bb48f
ubunturifa <jstrfaheem065@gmail.com>
2019-06-30 22:13:15 +0530

    # https://www.hackerrank.com/challenges/python-lists/problem,

--- HackerRankStudy/Lists.py
+++ HackerRankStudy/Lists.py
@@ -1,22 +1,23 @@
 # https://www.hackerrank.com/challenges/python-lists/problem
 
-if __name__ == '__main__':
-    N = int(input())
-    result = []
-    for n in range(N):
-        x = input().split(" ")
-        command = x[0]
-        if command == 'append':
-            result.append(int(x[1]))
-        if command == 'print':
-            print(result)
-        if command == 'insert':
-            result.insert(int(x[1]), int(x[2]))
-        if command == 'reverse':
-            result.reverse()
-        if command == 'pop':
-            result.pop()
-        if command == 'sort':
-            result.sort()
-        if command == 'remove':
-            result.remove(int(x[1]))
+newList = []
+
+n = int(input())
+
+for _ in range(n):
+    userInput = input().split(" ")
+
+    if userInput[0] == "insert":
+        newList.insert(int(userInput[1]), int(userInput[2]))
+    elif userInput[0] == "print":
+        print(newList)
+    elif userInput[0] == "remove":
+        newList.remove(int(userInput[1]))
+    elif userInput[0] == "append":
+        newList.append(int(userInput[1]))
+    elif userInput[0] == "sort":
+        newList.sort()
+    elif userInput[0] == "pop":
+        newList.pop()
+    elif userInput[0] == "reverse":
+        newList.reverse()
