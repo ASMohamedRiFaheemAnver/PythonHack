f00064003ffabbf712279f2a0ed18baa78a4fb14
ubunturifa <jstrfaheem065@gmail.com>
2019-07-06 16:43:52 +0530

    https://www.hackerrank.com/challenges/swap-case/problem

---
+++ HackerRankStudy/sWAPcASE.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/swap-case/problem
+
+
+def swap_case(s):
+    a = ""
+    for let in s:
+        if let.isupper():
+            a += let.lower()
+        else:
+            a += let.upper()
+
+    return a
+
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
