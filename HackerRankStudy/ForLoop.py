e6260bc0be4f95e2e4fb9d61f5aeda3a5353f750
ubunturifa <jstrfaheem065@gmail.com>
2019-07-06 21:29:40 +0530

    https://www.hackerrank.com/challenges/whats-your-name/problem

--- HackerRankStudy/ForLoop.py
+++ HackerRankStudy/ForLoop.py
@@ -2,7 +2,7 @@ if __name__ == '__main__':
     n = int(input())
 
 counter = 0
-while(counter < n):
+while counter < n:
     print(counter*counter)
     counter = counter + 1
 # https://www.w3schools.com/python/python_for_loops.asp
