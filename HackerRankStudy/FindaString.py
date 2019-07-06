655d98a1514e34015c422aa56ab6b2afd60665b0
ubunturifa <jstrfaheem065@gmail.com>
2019-07-07 01:05:09 +0530

    https://www.hackerrank.com/challenges/find-a-string/problem

---
+++ HackerRankStudy/FindaString.py
@@ -0,0 +1,18 @@
+# https://www.hackerrank.com/challenges/find-a-string/problem
+
+
+def count_substring(string, sub_string):
+    x = 0
+    for i in range(0, len(string)):
+        if string[i:i+len(sub_string)] == sub_string:
+            x = x +1
+
+    return x
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
